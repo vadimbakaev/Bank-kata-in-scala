64dbc2f91c6c5d6d6627544fa818741b16ba2f74
vadimbakaev <bakaev.vad@gmail.com>
2018-01-07 12:57:56 +0100

    Add transfer feature

--- src/main/scala/bakaev/vad/Account.scala
+++ src/main/scala/bakaev/vad/Account.scala
@@ -5,20 +5,36 @@ import java.util.Objects
 
 import bakaev.vad.Amount.PositiveAmount
 
-class Account(private val sate: TransactionStorage, private val statePrinter: StatePrinter) {
+class Account(private val transactionStorage: TransactionStorage, private val statePrinter: StatePrinter) {
+  require(transactionStorage != null, "TransactionStorage cannot be null in Account")
+  require(statePrinter != null, "StatePrinter cannot be null in Account")
 
-  def printStatement(): Unit = sate.printStatements(statePrinter)
+  def deposit(amount: PositiveAmount, date: LocalDate): Account = {
+    require(amount != null, "Amount cannot be null in deposit")
+    require(date != null, "Date cannot be null in deposit")
+    new Account(transactionStorage.deposit(amount, date), statePrinter)
+  }
 
-  def deposit(amount: PositiveAmount, date: LocalDate): Account =
-    new Account(sate.deposit(amount, date), statePrinter)
+  def withdraw(amount: PositiveAmount, date: LocalDate): Account = {
+    require(amount != null, "Amount cannot be null in withdraw")
+    require(date != null, "Date cannot be null in withdraw")
+    new Account(transactionStorage.withdraw(amount, date), statePrinter)
+  }
 
-  def withdraw(amount: PositiveAmount, date: LocalDate): Account =
-    new Account(sate.withdraw(amount, date), statePrinter)
+  def transfer(amount: PositiveAmount, date: LocalDate, receiver: Account): (Account, Account) = {
+    require(receiver != null, "Receiver cannot be null in transfer")
+    require(receiver != this, "Receiver cannot be sender in transfer")
+    require(amount != null, "Amount cannot be null in transfer")
+    require(date != null, "Date cannot be null in transfer")
+    (this.withdraw(amount, date), receiver.deposit(amount, date))
+  }
+
+  def printStatement(): Unit = transactionStorage.printStatements(statePrinter)
 
   override def equals(obj: scala.Any): Boolean = obj match {
-    case that: Account => sate == that.sate && statePrinter == that.statePrinter
+    case that: Account => transactionStorage == that.transactionStorage && statePrinter == that.statePrinter
     case _             => false
   }
 
-  override def hashCode: Int = Objects.hashCode(sate, statePrinter)
+  override def hashCode: Int = Objects.hashCode(transactionStorage, statePrinter)
 }
--- src/test/scala/bakaev/vad/unit/AccountTest.scala
+++ src/test/scala/bakaev/vad/unit/AccountTest.scala
@@ -2,9 +2,10 @@ package bakaev.vad.unit
 
 import java.time.LocalDate
 
-import bakaev.vad.Amount.PositiveAmount
-import bakaev.vad.{Account, Amount, StatePrinter, TransactionStorage}
-import org.mockito.Mockito
+import bakaev.vad.Amount.{NegativeAmount, PositiveAmount}
+import bakaev.vad.Transaction.{Credit, Debit}
+import bakaev.vad.{Account, StatePrinter, TransactionStorage}
+import org.mockito.Mockito._
 import org.scalatest.mockito.MockitoSugar
 import org.scalatest.{FlatSpec, Matchers}
 
@@ -16,9 +17,11 @@ class AccountTest extends FlatSpec with Matchers with MockitoSugar {
 
     val account = new Account(state, statePrinter)
 
+    when(state.deposit(new PositiveAmount(1000), LocalDate.MIN)).thenReturn(TransactionStorage())
+
     account.deposit(new PositiveAmount(1000), LocalDate.MIN) should not equal account
 
-    Mockito.verify(state).deposit(new PositiveAmount(1000), LocalDate.MIN)
+    verify(state).deposit(new PositiveAmount(1000), LocalDate.MIN)
   }
 
   it should "store deposit on state and return new account" in {
@@ -27,6 +30,7 @@ class AccountTest extends FlatSpec with Matchers with MockitoSugar {
 
     val account = new Account(state, statePrinter)
 
+    when(state.deposit(new PositiveAmount(1000), LocalDate.MIN)).thenReturn(TransactionStorage())
     account.deposit(new PositiveAmount(1000), LocalDate.MIN) should not equal account
   }
 
@@ -36,9 +40,10 @@ class AccountTest extends FlatSpec with Matchers with MockitoSugar {
 
     val account = new Account(state, statePrinter)
 
+    when(state.withdraw(new PositiveAmount(1000), LocalDate.MIN)).thenReturn(TransactionStorage())
     account.withdraw(new PositiveAmount(1000), LocalDate.MIN)
 
-    Mockito.verify(state).withdraw(new PositiveAmount(1000), LocalDate.MIN)
+    verify(state).withdraw(new PositiveAmount(1000), LocalDate.MIN)
   }
 
   it should "store withdraw on state and return new account" in {
@@ -46,8 +51,51 @@ class AccountTest extends FlatSpec with Matchers with MockitoSugar {
     val statePrinter = mock[StatePrinter]
 
     val account = new Account(state, statePrinter)
+    when(state.withdraw(new PositiveAmount(1000), LocalDate.MIN)).thenReturn(TransactionStorage())
 
     account.withdraw(new PositiveAmount(1000), LocalDate.MIN) should not equal account
+  }
+
+  it should "transfer amount to an account, return accounts updated with transactions" in {
+    val statePrinter = mock[StatePrinter]
+    val fstAccountState = mock[TransactionStorage]
+    val sndAccountState = mock[TransactionStorage]
+
+    val fstAccount = new Account(fstAccountState, statePrinter)
+    val sndAccount = new Account(sndAccountState, statePrinter)
+
+    val fstUpdatedTransactionStorage = TransactionStorage(Seq(new Debit(new NegativeAmount(-100), LocalDate.MIN)))
+    val sndUpdatedTransactionStorage = TransactionStorage(Seq(new Credit(new PositiveAmount(100), LocalDate.MIN)))
+
+    when(fstAccountState.withdraw(new PositiveAmount(100), LocalDate.MIN)).thenReturn(fstUpdatedTransactionStorage)
+    when(sndAccountState.deposit(new PositiveAmount(100), LocalDate.MIN)).thenReturn(sndUpdatedTransactionStorage)
+
+    val (fstUpdatedAccount, sndUpdatedAccount) = fstAccount.transfer(new PositiveAmount(100), LocalDate.MIN, sndAccount)
+
+    fstUpdatedAccount should equal (new Account(fstUpdatedTransactionStorage, statePrinter))
+    sndUpdatedAccount should equal (new Account(sndUpdatedTransactionStorage, statePrinter))
+  }
+
+  it should "throw an exception if receiver is null" in {
+    val statePrinter = mock[StatePrinter]
+    val fstAccountState = mock[TransactionStorage]
+
+    val fstAccount = new Account(fstAccountState, statePrinter)
+
+    the[IllegalArgumentException] thrownBy {
+      fstAccount.transfer(new PositiveAmount(100), LocalDate.MIN, null)
+    } should have message "requirement failed: Receiver cannot be null in transfer"
+  }
+
+  it should "throw an exception if sender is receiver" in {
+    val statePrinter = mock[StatePrinter]
+    val fstAccountState = mock[TransactionStorage]
+
+    val fstAccount = new Account(fstAccountState, statePrinter)
+
+    the[IllegalArgumentException] thrownBy {
+      fstAccount.transfer(new PositiveAmount(100), LocalDate.MIN, fstAccount)
+    } should have message "requirement failed: Receiver cannot be sender in transfer"
   }
 
   it should "print statement" in {
@@ -58,7 +106,7 @@ class AccountTest extends FlatSpec with Matchers with MockitoSugar {
 
     account.printStatement()
 
-    Mockito.verify(state).printStatements(statePrinter)
+    verify(state).printStatements(statePrinter)
   }
 
 }
