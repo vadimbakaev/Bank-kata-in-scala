3a8db13739fbce514f1d22dcdc60bef466778b27
vadimbakaev <bakaev.vad@gmail.com>
2018-01-07 18:47:10 +0100

    Add State trait

--- src/main/scala/bakaev/vad/State.scala
+++ src/main/scala/bakaev/vad/State.scala
@@ -5,16 +5,27 @@ import java.util.Objects
 
 import bakaev.vad.Amount.{NegativeAmount, NotZeroAmount, PositiveAmount}
 
-class State(private val localDate: LocalDate, private val operation: NotZeroAmount, private val balance: Amount) {
-  def printOn(printer: StatePrinter): Unit = operation match {
-    case positive: PositiveAmount => printer printLine (localDate, positive, balance)
-    case negative: NegativeAmount => printer printLine (localDate, negative, balance)
+trait State {
+  def printOn(printer: StatePrinter): Unit
+}
+
+object State {
+  def apply(localDate: LocalDate, operation: NotZeroAmount, balance: Amount): State =
+    new StateImpl(localDate, operation, balance)
+
+  class StateImpl(private val localDate: LocalDate, private val operation: NotZeroAmount, private val balance: Amount)
+      extends State {
+    def printOn(printer: StatePrinter): Unit = operation match {
+      case positive: PositiveAmount => printer printLine (localDate, positive, balance)
+      case negative: NegativeAmount => printer printLine (localDate, negative, balance)
+    }
+
+    override def equals(obj: scala.Any): Boolean = obj match {
+      case that: StateImpl => localDate == that.localDate && operation == that.operation
+      case _               => false
+    }
+
+    override def hashCode: Int = Objects.hashCode(localDate, operation)
   }
 
-  override def equals(obj: scala.Any): Boolean = obj match {
-    case that: State => localDate == that.localDate && operation == that.operation
-    case _           => false
-  }
-
-  override def hashCode: Int = Objects.hashCode(localDate, operation)
 }
